1c23a00270a4ac853c89be0f609de3f3a7336029
ricardev <ricardev@ricar.dev>
2023-12-09 21:03:53 +0100

    minor changes

--- personalization.sh
+++ personalization.sh
@@ -84,6 +84,7 @@ function qtile(){
     mkdir -p $HOME/.config/qtile
     cp -r ./qtile_files/* ~/.config/qtile
     mv ~/.config/qtile/picom.conf ~/.config/
+    chmod +x ~/.config/qtile/autostart.sh
 
     # Creating configuration files for root
     sudo mkdir -p root/.config/qtile
